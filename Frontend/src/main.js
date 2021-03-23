953058cb436aef15596b8f496dcd7ea2cdc599c7
serdyuuuk <fedyaserdyuk@gmail.com>
2021-03-23 20:22:43 +0200

    menu

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,1 +1,9 @@
-console.log("work!");
+$("#openMenuButton").click(function () {
+   if($("#menu").hasClass("menuHidden")) {
+      $("#menu").removeClass("menuHidden");
+      $("#menu").addClass("menuVisible");
+   } else {
+      $("#menu").removeClass("menuVisible");
+      $("#menu").addClass("menuHidden");
+   }
+});
