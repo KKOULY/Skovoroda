184bfbea36b9d473a06667d547da8eac5c8d3aa1
serdyuuuk <fedyaserdyuk@gmail.com>
2021-03-23 21:08:43 +0200

    buttons and animation

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,9 +1,11 @@
 $("#openMenuButton").click(function () {
-   if($("#menu").hasClass("menuHidden")) {
-      $("#menu").removeClass("menuHidden");
-      $("#menu").addClass("menuVisible");
-   } else {
-      $("#menu").removeClass("menuVisible");
-      $("#menu").addClass("menuHidden");
-   }
+    if ($("#menu").hasClass("menuHidden")) {
+        $("#menu").removeClass("menuHidden");
+        $("#menu").addClass("menuVisible");
+        $(".buttonInMenu").fadeIn(200);
+    } else {
+        $("#menu").removeClass("menuVisible");
+        $("#menu").addClass("menuHidden");
+        $(".buttonInMenu").fadeOut(200);
+    }
 });
