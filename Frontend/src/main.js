4e0c87d0c65aaea012b6c555a72eae77bb46ed07
KKOULY <dmt.solovyov@gmail.com>
2021-03-26 01:52:06 +0200

    fix

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,9 +1,11 @@
 $("#openMenuButton").click(function () {
     if ($("#menu").hasClass("menuHidden")) {
+        $("body").addClass("modal-open");
         $("#menu").removeClass("menuHidden");
         $("#menu").addClass("menuVisible");
         $(".buttonInMenu").fadeIn(200);
     } else {
+        $("body").removeClass("modal-open");
         $("#menu").removeClass("menuVisible");
         $("#menu").addClass("menuHidden");
         $(".buttonInMenu").fadeOut(200);
