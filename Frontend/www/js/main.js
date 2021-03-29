f0e84558a0b7b082e44cf4a78470b94640501848
serdyuuuk <fedyaserdyuk@gmail.com>
2021-03-29 21:45:04 +0300

    fix

--- Frontend/www/js/main.js
+++ Frontend/www/js/main.js
@@ -12,4 +12,16 @@ $("#openMenuButton").click(function () {
         $(".buttonInMenu").fadeOut(200);
     }
 });
+
+document.onmousemove = function (){
+    document.getElementsByTagName('html')[0].insertAdjacentHTML('beforeEnd', '<img src="images/cursor.svg" id="curs">');
+    var curs = document.getElementById('curs');
+    curs.style.zIndex = '5';
+    curs.style.position = 'fixed';
+    curs.style.pointerEvents = 'none';
+    document.onmousemove = function(event){
+        curs.style.left = event.clientX -9+'px';
+        curs.style.top = event.clientY -9+'px';
+    }
+}
 },{}]},{},[1]);
