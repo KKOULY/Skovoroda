2be1cc4fabd693aa8162061f421cc68adac4b976
BRomanchuk <47035610+BRomanchuk@users.noreply.github.com>
2021-04-24 18:59:05 +0300

    upd list of compositions

--- Backend/data/CompositionList.js
+++ Backend/data/CompositionList.js
@@ -17,51 +17,159 @@ const compositionList = [
     },
     {
         id: 3,
-        title: 'бджола і шершень',
+        title: 'бджола та шершень',
         icon: 'images/library/13.jpg',
-        text: ''
+        text: 'c3.txt'
     },
     {
         id: 4,
-        title: 'сад божествених пісень',
+        title: 'Думка, мандруючи, співає пісню',
         icon: 'images/library/14.jpg',
-        text: ''
+        text: 'c4.txt'
     },
     {
         id: 5,
-        title: 'composition',
+        title: 'Все лице морщиш, журний завсігди ти...',
         icon: 'images/library/alt.jpg',
-        text: ''
+        text: 'c5.txt'
     },
     {
         id: 6,
-        title: 'composition',
+        title: 'Ось вона, молодість року! Природи лице оновилось',
         icon: 'images/library/alt.jpg',
-        text: ''
+        text: 'c6.txt'
     },
     {
         id: 7,
-        title: 'composition',
+        title: 'Гей, поля, поля зелені...',
         icon: 'images/library/alt.jpg',
-        text: ''
+        text: 'c7.txt'
     },
     {
         id: 8,
-        title: 'composition',
+        title: "музам колись дев'ятьом на шляху...",
         icon: 'images/library/alt.jpg',
-        text: ''
+        text: 'c8.txt'
     },
     {
         id: 9,
-        title: 'composition',
+        title: 'Жаби',
         icon: 'images/library/alt.jpg',
-        text: ''
+        text: 'c9.txt'
     },
     {
         id: 10,
-        title: 'composition',
+        title: 'Зозуля та дрізд',
         icon: 'images/library/alt.jpg',
-        text: ''
+        text: 'c10.txt'
+    },
+    {
+        id: 11,
+        title: 'На день народження Василя Томари',
+        icon: 'images/library/alt.jpg',
+        text: 'c11.txt'
+    },
+    {
+        id: 12,
+        title: 'Облудники, що моляться, співають',
+        icon: 'images/library/alt.jpg',
+        text: 'c12.txt'
+    },
+    {
+        id: 13,
+        title: 'Гей ти, пташко жовтобоко',
+        icon: 'images/library/alt.jpg',
+        text: 'c13.txt'
+    },
+    {
+        id: 14,
+        title: 'Фабула про тантала',
+        icon: 'images/library/alt.jpg',
+        text: 'c14.txt'
+    },
+    {
+        id: 15,
+        title: 'Похвала бідності',
+        icon: 'images/library/alt.jpg',
+        text: 'c15.txt'
+    },
+    {
+        id: 16,
+        title: 'В город не піду багатий — на полях я буду жить',
+        icon: 'images/library/alt.jpg',
+        text: 'c16.txt'
+    },
+    {
+        id: 17,
+        title: 'То яка ж та слава нині',
+        icon: 'images/library/alt.jpg',
+        text: 'c17.txt'
+    },
+    {
+        id: 18,
+        title: "Вже хмара пройшла. Райдуга в небі грає",
+        icon: 'images/library/alt.jpg',
+        text: 'c18.txt'
+    },
+    {
+        id: 19,
+        title: 'Бачачи життя осього горе',
+        icon: 'images/library/alt.jpg',
+        text: 'c19.txt'
+    },
+    {
+        id: 20,
+        title: 'Гей ти, нудьго проклята! Ся докучлива печаль',
+        icon: 'images/library/alt.jpg',
+        text: 'c20.txt'
+    },
+    {
+        id: 21,
+        title: 'Чистий можеш буть собою',
+        icon: 'images/library/alt.jpg',
+        text: 'c21.txt'
+    },
+    {
+        id: 22,
+        title: 'Щастя, а де ти живеш? Горлиці, скажіте!',
+        icon: 'images/library/alt.jpg',
+        text: 'c22.txt'
+    },
+    {
+        id: 23,
+        title: 'То яка ж та слава нині',
+        icon: 'images/library/alt.jpg',
+        text: 'c23.txt'
+    },
+    {
+        id: 24,
+        title: "Вже хмара пройшла. Райдуга в небі грає",
+        icon: 'images/library/alt.jpg',
+        text: 'c24.txt'
+    },
+    {
+        id: 25,
+        title: 'Бачачи життя осього горе',
+        icon: 'images/library/alt.jpg',
+        text: 'c25.txt'
+    },
+    {
+        id: 26,
+        title: 'Гей ти, нудьго проклята! Ся докучлива печаль',
+        icon: 'images/library/alt.jpg',
+        text: 'c26.txt'
+    },
+    {
+        id: 27,
+        title: 'Чистий можеш буть собою',
+        icon: 'images/library/alt.jpg',
+        text: 'c27.txt'
+    },
+    {
+        id: 28,
+        title: 'Щастя, а де ти живеш? Горлиці, скажіте!',
+        icon: 'images/library/alt.jpg',
+        text: 'c28.txt'
     }
 ]
 
